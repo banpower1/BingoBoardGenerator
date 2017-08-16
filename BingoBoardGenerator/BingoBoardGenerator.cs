2963f8227b1da8e10dc3ae58b7ea9acd568809aa
BNicolas <bertram@fam-nicolas.dk>
2017-08-16 18:13:34 +0200

    Initial file push

---
+++ BingoBoardGenerator/BingoBoardGenerator.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BingoBoardGenerator
+{
+    class BingoBoardGenerator
+    {
+        /**
+         * Number of boards
+         * Number of numbers per board
+         * Ask if the boards should contain blank spaces
+         * Where to get the words/numbers from (.CSV file)
+         */
+
+        HashSet<int>[] boards;
+        int numberOfBoards;
+        int numberPerBoard;
+        Random rnd;
+
+        public string[] bingoSquareContent;
+
+        public BingoBoardGenerator(int numberOfBoards, int numberPerBoard, string inputFilePath)
+        {
+            this.numberOfBoards = numberOfBoards;
+            this.numberPerBoard = numberPerBoard;
+            boards = new HashSet<int>[numberOfBoards];
+
+            ReadInputFile(inputFilePath, out bingoSquareContent);
+        }
+
+        public void GenerateGame()
+        {
+            rnd = new Random();
+            boards[0] = GenerateBoard();
+            PrintBoardNumber(0, boards[0]);
+            //Run through each board to generate a board
+            for (int i = 1; i < boards.Length; i++)
+            {
+                bool noMatchBoardFound = true;
+                
+                //Keep attempting to make a board as long as we don't have a valid one
+                while (noMatchBoardFound)
+                {
+                    HashSet<int> board = GenerateBoard();
+
+                    //Check through the already generated boards if it exists
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (BoardEquals(board, boards[j]))
+                        {
+                            //A matching board was found, break out of the for J for loop because we don't need to continue
+                            break;
+                        }
+                        noMatchBoardFound = false;
+                    }
+
+                    //If no matching board was found, store the generated board, else reset the while loop and try again
+                    if (!noMatchBoardFound)
+                    {
+                        boards[i] = board;
+                        PrintBoardNumber(i, board);
+                    }
+                }
+            }
+            HashSet<string>[] finalBoards;
+            ConvertBoard(boards, out finalBoards);
+            WriteOutputFile(finalBoards);
+            Console.ReadKey();
+        }
+
+        HashSet<int> GenerateBoard()
+        {
+            HashSet<int> board = new HashSet<int>();
+            while (board.Count < numberPerBoard)
+            {
+                board.Add(rnd.Next(bingoSquareContent.Length));
+            }
+            return board;
+        }
+
+        bool BoardEquals(HashSet<int> board1, HashSet<int> board2)
+        {
+            foreach (int x in board1)
+            {
+                if (!board2.Contains(x))
+                {
+                    return false;
+                }
+            }
+
+            Console.WriteLine("\nFUCK");
+            return true;
+        }
+
+        void PrintBoardNumber(int boardNumber, HashSet<int> board)
+        {
+            Console.Write("\n " + boardNumber + ": " + board.First());
+            for (int i = 1; i < board.Count; i++)
+            {
+                Console.Write(", {0}", board.Skip(i).First());
+            }
+        }
+
+        void ConvertBoard(HashSet<int>[] input, out HashSet<string>[] output)
+        {
+            output = new HashSet<string>[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[i] = new HashSet<string>();
+                foreach (int x in input[i])
+                {
+                    output[i].Add(bingoSquareContent[x]);
+                }
+            }
+        }
+
+        void ReadInputFile(string inputFileName, out string[] output)
+        {
+            output = new string[0];
+            string codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            string executionPath = Path.GetDirectoryName(path);
+            try
+            {
+                output = File.ReadAllLines(executionPath + "\\" + inputFileName);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found at the following path: {0}\\{1}", executionPath, inputFileName);
+                Console.ReadLine();
+            }
+            //use the length of the array to know how many different "numbers" you have (regular bingo is 1-90)
+        }
+
+        void WriteOutputFile(HashSet<string>[] boards)
+        {
+            string[] output = new string[boards.Length];
+            for (int i = 0; i < boards.Length; i++)
+            {
+                string outputLine = "";
+                foreach (string x in boards[i])
+                {
+                    outputLine += x + ", ";
+                }
+
+                outputLine = outputLine.Remove(outputLine.Length - 2,2);
+                
+                output[i] = outputLine;
+            }
+
+            try
+            {
+                string codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                string executionPath = Path.GetDirectoryName(path);
+                File.WriteAllLines(executionPath + "\\" + "output.csv",output);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ BingoBoardGenerator/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+/***
+* Ask user for input
+* -Number of boards
+* -Number of numbers per board
+* -Ask if the boards should contain blank spaces
+* -Where to get the words/numbers from (.CSV file)
+* 
+* Generate boards
+* -
+***/
+
+namespace BingoBoardGenerator
+{
+    class Program
+    {
+        enum printMessage
+        {
+            WelcomeScreen, 
+            MainMenu,
+            GoodBye
+        }
+
+
+        static void Main(string[] args)
+        {
+            bool mainMenu = true;
+            while (mainMenu)
+            {
+
+
+                bool waitingForInput = true;
+                Regex rgxDigit = new Regex(@"D[0-9]");
+                Regex rgxNumPad = new Regex(@"NumPad[0-9]");
+
+                while (waitingForInput)
+                {
+                    Console.Clear();
+                    Print(printMessage.WelcomeScreen);
+                    Print(printMessage.MainMenu);
+                    ConsoleKeyInfo input = Console.ReadKey(true);
+                    string inputString = input.Key.ToString();
+                    
+
+                    if (rgxDigit.IsMatch(inputString) || rgxNumPad.IsMatch(inputString))
+                    {
+                        waitingForInput = false;
+                        string inputNumber = Regex.Replace(input.Key.ToString(), @"[^0-9]", "");
+
+                        switch (inputNumber)
+                        {
+                            case "0":
+                                mainMenu = false;
+                                Print(printMessage.GoodBye);
+                                break;
+                            case "1":
+
+                                //int numberOfBoards, int numberPerBoard, string inputFilePath, int boardWidth, int boardHeight
+                                Console.WriteLine("Please enter the following information:");
+                                int numberOfBoards;
+                                int numberPerBoard;
+                                string inputFilePath;
+                                do
+                                {
+                                    Console.Write("Number of unique boards to generate: ");
+                                } while (!int.TryParse(Console.ReadLine(), out numberOfBoards));
+                                do
+                                {
+                                    Console.Write("Amount of \"Numbers\" per board: ");
+                                } while (!int.TryParse(Console.ReadLine(), out numberPerBoard));
+
+                                Console.Write("Filename: ");
+                                inputFilePath = Console.ReadLine();
+
+                                BingoBoardGenerator generator = new BingoBoardGenerator(numberOfBoards, numberPerBoard, inputFilePath);
+
+                                generator.GenerateGame();
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Print(printMessage option)
+        {
+            switch (option)
+            {
+                case printMessage.WelcomeScreen:
+                    Console.WriteLine("Copyright \u00a9 Bertram André Nicolas - https://github.com/banpower1");
+                    Console.WriteLine("");
+                    Console.WriteLine("This is the Bingo Board Generator.");
+                    break;
+                case printMessage.MainMenu:
+                    Console.WriteLine("1. - Generate plates");
+                    Console.WriteLine("0. - Exit");
+                    break;
+                case printMessage.GoodBye:
+                    Console.WriteLine("\nThis was a triumph!\nI'm making a note here:\nHuge success!");
+                    System.Threading.Thread.Sleep(3000);
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+    }
+}
